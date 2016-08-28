7b45d98cce197fa476a6e21c05b8c117a071e8cd
didoogan <tzd0409@gmail.com>
2016-08-29 00:12:59 +0300

    fucking errors

--- app/app.js
+++ app/app.js
@@ -1,31 +1,69 @@
-
+ // python -m SimpleHTTPServer  -   for running through http://localhost:8000
 var myApp = angular.module('myApp', ['ngRoute']);
 
 myApp.config(function ($routeProvider) {
   $routeProvider
 
       .when('/first', {
-        templateUrl: '../templates/first.html',
+        templateUrl: 'templ/1.html',
         controller: 'mainController'
       })
 
       .when('/second', {
-        templateUrl: '../templates/second.html',
+        templateUrl: 'templ/2.html',
         controller: 'secondController'
       })
 });
-myApp.controller('mainController', ['$scope',
-  function($scope) {
 
+myApp.service('myService', function() {
+    var self = this;
+    this.name = 'diDoogan';
+    this.namelength = function() {
+        return self.name.length;
+    };
+});
 
+myApp.controller('mainController', ['$scope', '$routeParams', 'myService', '$log',
+  function($scope, $routeParams, myService, $log) {
+    $scope.name = myService.name;
+    $scope.$watch('name', function() {
+        myService.name = $scope.name;
+    });
+    $scope.data = {
+        link: "http//:supersite.com",
+        number: 1,
+        string: "Just about ..."
+    };
 }]);
 
 
-myApp.controller('secondController', ['$scope',
-  function($scope) {
-
+myApp.controller('secondController', ['$scope', '$routeParams', 'myService',
+  function($scope, $routeParams, myService) {
+    $scope.name = myService.name;
+    $scope.$watch('name', function() {
+        myService.name = $scope.name;
+    });
+    $scope.name = myService.name;
+    // $scope. var = $routeParams.var || 1;
 
 }]);
+
+myApp.directive('superTag', function() {
+    return {
+        restrict: "AECM",
+        templateUrl: "directives/supertag.html"
+        // ,
+        // scope: {
+        //     dataLink: "@",
+        //     dataNumber: "@",
+        //     dataString: "@"
+        // }
+        // , replace: true
+        , scope: {
+            dataObject: "="
+        }
+    }
+}) ;
 
 
 
